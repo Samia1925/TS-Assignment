740a533a8c09e50e61739eb295ce547f6444e5e3
Samia Jahan <101088284+Samia1925@users.noreply.github.com>
2025-01-07 15:06:01 +0600

    Create 5_oddEven.js

---
+++ 5_oddEven.js
@@ -0,0 +1,9 @@
+"use strict";
+let num = parseInt(prompt("Enter the number to check odd or even") || "0");
+if (num % 2 === 0) {
+    console.log(num + " is even");
+}
+else {
+    console.log(num + " is odd");
+}
+//# sourceMappingURL=5_oddEven.js.map

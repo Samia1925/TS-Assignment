a502482923cc8f9d40c07cf0f8a5b167fde0aecf
Samia Jahan <101088284+Samia1925@users.noreply.github.com>
2025-01-07 16:21:14 +0600

    Create 11_Quad_Eqn.ts

---
+++ 11_Quad_Eqn.ts
@@ -0,0 +1,22 @@
+let a: number = parseInt(prompt("Value of a: ") || "0");
+let b: number = parseInt(prompt("Value of b: ") || "0");
+let c: number = parseInt(prompt("Value of c: ") || "0");
+let x: number;
+let temp: number;
+if (a === 0 && b === 0 && c === 0) {
+    console.log("No Solution");
+} else if (((b * b) - 4 * a * c) <= 0) {
+    console.log("Roots are imaginary");
+} else if (a === 0) {
+    x = -c / b;
+    console.log("The value is " + x);
+} else if (b === 0) {
+    temp = -c / a;
+    x = Math.sqrt(temp);
+    console.log("The value is " + x);
+} else {
+    let d = b * b - 4 * a * c;
+    let root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+    console.log("Roots are: " + root1 + " and " + root2);
+}

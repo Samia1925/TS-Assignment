5ad82221439f9b56bbf90cdf936e0d3cb5471ef2
Samia Jahan <101088284+Samia1925@users.noreply.github.com>
2025-01-07 15:01:46 +0600

    Create 4_lineEqn.js

---
+++ 4_lineEqn.js
@@ -0,0 +1,6 @@
+"use strict";
+let x1 = 2, y1 = 2, x2 = 5, y2 = 6;
+let slope = (y2 - y1) / (x2 - x1);
+let point = y1 - slope * x1;
+console.log(slope + " x  " + point);
+//# sourceMappingURL=4_lineEqn.js.map

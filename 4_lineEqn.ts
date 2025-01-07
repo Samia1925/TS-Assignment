d625cd39fb3923eeccbbd3db23af8cbcbe5db926
Samia Jahan <101088284+Samia1925@users.noreply.github.com>
2025-01-07 15:00:44 +0600

    Create 4_lineEqn.ts

---
+++ 4_lineEqn.ts
@@ -0,0 +1,4 @@
+let x1: number=2, y1: number=2, x2: number=5, y2: number=6;
+        let slope: number= (y2-y1)/(x2-x1);
+        let point: number=y1- slope*x1;
+        console.log(slope +" x  "+ point); 

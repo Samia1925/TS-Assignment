ddf2612e9d81bf5688d2a263979a54199fd06224
Samia Jahan <101088284+Samia1925@users.noreply.github.com>
2025-01-07 16:47:00 +0600

    Create 16_isPrime.js

---
+++ 16_isPrime.js
@@ -0,0 +1,18 @@
+"use strict";
+function isPrime(num) {
+    if (num <= 1)
+        return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0)
+            return false;
+    }
+    return true;
+}
+let num = parseInt(prompt("Enter the number:") || "0");
+if (isPrime(num) == true) {
+    console.log(num + " is prime");
+}
+else {
+    console.log(num + " is not prime");
+}
+//# sourceMappingURL=16_isPrime.js.map
